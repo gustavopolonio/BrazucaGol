2a4c24faf86127e2338549a5ef80b17531c68689
Gustavo <gustavopolonio1@gmail.com>
2022-05-03 13:36:09 -0300

    menu sidebar first version

--- src/components/MenuSidebar/index.tsx
+++ src/components/MenuSidebar/index.tsx
@@ -1,3 +1,5 @@
+import { MdPlayArrow } from "react-icons/md"
+
 import styles from './styles.module.scss'
 
 export function MenuSidebar() {
@@ -7,18 +9,18 @@ export function MenuSidebar() {
       <h2>Menu</h2>
 
       <nav>
-        <a href="">Principal</a>
-        <a href="">Rodadas</a>
-        <a href="">Níveis</a>
-        <a href="">Premiações</a>
-        <a href="">Ranking</a>
-        <a href="">Regras</a>
-        <a href="">Dúvidas</a>
-        <a href="">Divisão de Acesso</a>
-        <a href="">Divisão de Jogadores</a>
-        <a href="">Movimentações</a>
-        <a href="">Vips</a>
-        <a href="">Confirmar VIP</a>
+        <a href="">Principal<MdPlayArrow /><div></div></a>
+        <a href="">Rodadas<MdPlayArrow /><div></div></a>
+        <a href="">Níveis<MdPlayArrow /><div></div></a>
+        <a href="">Premiações<MdPlayArrow /><div></div></a>
+        <a href="">Ranking<MdPlayArrow /><div></div></a>
+        <a href="">Regras<MdPlayArrow /><div></div></a>
+        <a href="">Dúvidas<MdPlayArrow /><div></div></a>
+        <a href="">Divisão de Acesso<MdPlayArrow /><div></div></a>
+        <a href="">Divisão de Jogadores<MdPlayArrow /><div></div></a>
+        <a href="">Movimentações<MdPlayArrow /><div></div></a>
+        <a href="">Vips<MdPlayArrow /><div></div></a>
+        <a href="">Confirmar VIP<MdPlayArrow /><div></div></a>
       </nav>
     </aside>
   )
