e0cba363fd464e245faeb6d294ec00f37e535a17
Gustavo <gustavopolonio1@gmail.com>
2023-05-22 17:00:21 -0300

    fix: build errors

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
+  // eslint-disable-next-line no-unused-vars
   interface Session {
     isAvatarActive: boolean
   }
